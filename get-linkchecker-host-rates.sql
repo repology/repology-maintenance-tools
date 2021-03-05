72dd0ddfe5465475cfe131439a8cff067f95fc1a
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-03-05 23:47:10 +0300

    Update get-linkchecker-host-rates

--- get-linkchecker-host-rates.sql
+++ get-linkchecker-host-rates.sql
@@ -1,29 +1,36 @@
-\echo Top 10 hosts by URL count
+\echo Top 10 hosts by total URLs
 SELECT
 	substring(url from '.*://([^/]*)') AS "Host",
 	count(*) AS "Total URLs",
-	count(*) FILTER (WHERE next_check < now()) AS "Queued URLs"
+	count(*) FILTER(WHERE priority) AS "Priority URLs"
 FROM links
 WHERE refcount > 0
 GROUP BY "Host"
 ORDER BY "Total URLs" DESC
 LIMIT 10;
 
+\echo Top 10 hosts by queued URLs
+SELECT
+	substring(url from '.*://([^/]*)') AS "Host",
+	count(*) FILTER (WHERE next_check < now()) AS "Queued URLs",
+	count(*) FILTER (WHERE next_check < now() AND priority) AS "Priority URLs"
+FROM links
+WHERE refcount > 0
+GROUP BY "Host"
+ORDER BY "Queued URLs" DESC
+LIMIT 10;
+
 \echo Top 10 hosts by rate
 SELECT
 	substring(url from '.*://([^/]*)') AS "Host",
 	round(
-		count(*) / avg(
-			CASE WHEN next_check > now()
-				THEN extract(epoch FROM next_check - now())
-				ELSE 0
-			END
+		(
+			count(*) / extract(epoch FROM max(next_check) - now())
 		)::numeric,
 		3
 	) AS "Estimated RPS"
 FROM links
 WHERE refcount > 0
 GROUP BY "Host"
-HAVING count(*) > 1000
 ORDER BY "Estimated RPS" DESC
 LIMIT 10;
